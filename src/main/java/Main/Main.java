21f30a70ac65e480f5b45bff444cf6c2ea42a9bd
seniyas <seniyadissanayake@gmail.com>
2020-03-12 06:33:52 +0530

    basic classes

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,30 @@
 package Main;
 
-public class Main {
+import classes.Seat;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main extends Application {
+    private static final int SEATING_CAPACITY = 42;
+    Map<Integer, Seat> seatCollection = new HashMap<Integer,Seat>();
+
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        char menuSelection = scn.nextLine().charAt(0);
+        switch (menuSelection){
+            case 'A':
+                break;
+            case 'V':
+
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
 
     }
 }
--- src/main/java/classes/Seat.java
+++ src/main/java/classes/Seat.java
@@ -1,4 +1,19 @@
 package classes;
 
 public class Seat {
+    private static String cName;
+    private static String cNIC;
+    private static boolean isBooked;
+    private static int seatNum;
+
+    Seat(int num){
+        isBooked = false;
+        seatNum = num;
+    }
+
+    public static void assignCustomer(String Name,String NIC){
+        cName = Name;
+        cNIC = NIC;
+        isBooked = true;
+    }
 }
